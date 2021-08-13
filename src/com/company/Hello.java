43cb3d5dbcc1859779be46616e51f6f328777e52
Manny Lee <many221@gmail.com>
2021-08-12 17:09:32 -0700

    part 2

---
+++ src/com/company/Hello.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Hello {
+
+    public static void helloUSer(){
+        System.out.print ("Please Enter Your Name: ");
+        String name = Main.input.next ();
+        System.out.println ("Hello " + name);
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,47 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+public static Scanner input = new Scanner ( System.in );
+
     public static void main(String[] args) {
-	// write your code here
+//
+//        //Part 1`
+//        System.out.print("Please Enter Name: ");
+//        String name = input.nextLine ();
+//
+//        System.out.println ("Hello " + name);
+//        //----------------------------------
+
+        //Part 2
+        boolean check = true;
+
+        System.out.println ( "Welcome to Manny's CLI" );
+
+    while (check){
+        System.out.println ( "Please Choose an option below" );
+        System.out.println (
+                "1) Say Hello\n" +
+                        "2) Reverse a string\n" +
+                        "3) Add two numbers\n" +
+                        "4) Exit the program" );
+
+        System.out.print ( "Selection: " );
+        byte option = input.nextByte ();
+
+        switch (option) {
+            case 1 -> {
+                Hello.helloUSer ();
+            }
+            case 2, 3 -> System.out.println ( "Program still in development" );
+            case 4 -> {
+                check = false;
+                System.out.println ( "Bye-Bye! See You Soon" );
+            }
+            default -> System.out.println ( "Invalid option. Please choose from 1-4." );
+        }
+    }
     }
 }
